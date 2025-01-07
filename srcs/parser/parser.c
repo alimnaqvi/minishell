317907ee211970e7357dcc25b73e82255888c815
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-01-07 15:06:39 +0100

    complete first pass at parser code

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: anaqvi <anaqvi@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 13:12:05 by anaqvi            #+#    #+#             */
-/*   Updated: 2025/01/06 21:08:59 by anaqvi           ###   ########.fr       */
+/*   Updated: 2025/01/07 15:05:02 by anaqvi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -282,17 +282,80 @@ static int	update_cmd_grp_cmds(t_minishell *minishell, int *i, t_cmd_grp *cmd_gr
 		cmd_grp_node->cmd_type = EXTERNAL;
 	else
 		cmd_grp_node->cmd_type = BUILTIN;
-	cmd_grp_node->cmd_name = minishell->tokenized[cmd_start];
+	cmd_grp_node->cmd_name = ft_strdup(minishell->tokenized[cmd_start]);
 	cmd_grp_node->cmd_args = copy_cmd_args(minishell, arg_count, cmd_start,
 		cmd_grp_node);
-	if (!(cmd_grp_node->cmd_args))
+	if (!(cmd_grp_node->cmd_name) || !(cmd_grp_node->cmd_args))
 		return (-1);
+	gc_add_to_allocs(cmd_grp_node->cmd_name, minishell);
 	return (0);
+}
+
+static char	**get_paths(char **mini_env, t_minishell *minishell)
+{
+	char	**paths;
+	int		i;
+
+	if (!mini_env)
+		return (NULL);
+	while (*mini_env)
+	{
+		if (!ft_strncmp("PATH=", *mini_env, 5))
+			break ;
+		mini_env++;
+	}
+	if (!(*mini_env))
+		return (NULL);
+	paths = ft_split((*mini_env) + 5, ':');
+	if (!paths)
+		return (gc_exit(minishell, EXIT_FAILURE), NULL);
+	gc_add_to_allocs(paths, minishell);
+	i = 0;
+	while (paths[i])
+	{
+		gc_add_to_allocs(paths[i], mini_env);
+		i++;
+	}
+	return (paths);
+}
+
+static void	find_executable(t_cmd_grp *cmd_grp_node, char **paths, t_minishell *minishell)
+{
+	char	*current_path;
+	size_t	len;
+
+	while (*paths)
+	{
+		len = sizeof(char) * (ft_strlen(*paths) + ft_strlen(cmd_grp_node->cmd_name) + 2);
+		current_path = gc_malloc(len, minishell);
+		ft_strlcpy(current_path, *paths, len);
+		ft_strlcat(current_path, "/", len);
+		ft_strlcat(current_path, cmd_grp_node->cmd_name, len);
+		if (access(current_path, X_OK) == 0)
+			break ;
+		gc_free(current_path, minishell);
+		current_path = NULL;
+		paths++;
+	}
+	if (!current_path)
+		return ;
+	gc_free(cmd_grp_node->cmd_name, minishell);
+	cmd_grp_node->cmd_name = current_path;
+	return ;
 }
 
 static int	find_full_cmd_path(t_minishell *minishell, t_cmd_grp *cmd_grp_node)
 {
-	// replace cmd_grp_node->cmd_name with full path from env (for externals)
+	char	**paths;
+
+	if (cmd_grp_node->cmd_type == EXTERNAL)
+	{
+		if (!access(cmd_grp_node->cmd_name, X_OK))
+			return (0);
+		paths = get_paths(minishell->mini_env, minishell);
+		find_executable(cmd_grp_node, paths, minishell);
+	}
+	return (0);
 }
 
 int	parser(t_minishell *minishell)
