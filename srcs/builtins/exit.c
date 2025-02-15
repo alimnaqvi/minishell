0c5b1a8543377369ec4dccb4becea347df2193f6
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-02-15 12:30:07 +0100

    improve logic flow in exit builtin to mimic bash

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: anaqvi <anaqvi@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 19:26:56 by anaqvi            #+#    #+#             */
-/*   Updated: 2025/02/14 16:09:52 by anaqvi           ###   ########.fr       */
+/*   Updated: 2025/02/15 12:27:42 by anaqvi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,22 @@ void ft_exit_parent(char **args, t_minishell *minishell)
 	if (!args || !(*args))
 		return(gc_exit(minishell, EXIT_FAILURE));
 	argc = get_array_size(args);
-	if (argc > 2)
-	{
-		minishell->last_exit_status = EXIT_FAILURE;
-		return (put_builtin_error("exit", "", "too many arguments"));
-	}
 	if (argc == 1)
 	{
 		printf("exit\n");
 		gc_exit(minishell, minishell->last_exit_status);
 	}
 	if (ft_atoi_error(args[1], &exit_status_arg) == -1)
-		return (put_builtin_error("exit", args[1],
-			"numeric argument required"));
+	{
+		put_builtin_error("exit", args[1], "numeric argument required");
+		printf("exit\n");
+		gc_exit(minishell, 2);
+	}
+	if (argc > 2)
+	{
+		minishell->last_exit_status = EXIT_FAILURE;
+		return (put_builtin_error("exit", "", "too many arguments"));
+	}
 	printf("exit\n");
 	gc_exit(minishell, exit_status_arg);
 }
@@ -45,17 +48,17 @@ void ft_exit_child(char **args, t_minishell *minishell)
 	if (!args || !(*args))
 		return(gc_exit(minishell, EXIT_FAILURE));
 	argc = get_array_size(args);
-	if (argc > 2)
-	{
-		minishell->last_exit_status = EXIT_FAILURE;
-		put_builtin_error("exit", "", "too many arguments");
-		return (gc_exit(minishell, EXIT_FAILURE));
-	}
 	if (argc == 1)
 		gc_exit(minishell, minishell->last_exit_status);
 	if (ft_atoi_error(args[1], &exit_status_arg) == -1)
 	{
 		put_builtin_error("exit", args[1], "numeric argument required");
+		return (gc_exit(minishell, EXIT_FAILURE));
+	}
+	if (argc > 2)
+	{
+		minishell->last_exit_status = EXIT_FAILURE;
+		put_builtin_error("exit", "", "too many arguments");
 		return (gc_exit(minishell, EXIT_FAILURE));
 	}
 	gc_exit(minishell, exit_status_arg);
